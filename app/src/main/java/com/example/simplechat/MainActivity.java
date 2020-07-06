4dc2a8330533d8338ba8bee224c40a19177123d2
Shefali Goel <shgoel@fb.com>
2020-07-06 08:52:24 -0700

    Added login with anonymous ParseUser and saved messages

--- app/src/main/java/com/example/simplechat/MainActivity.java
+++ app/src/main/java/com/example/simplechat/MainActivity.java
@@ -3,12 +3,79 @@ package com.example.simplechat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseAnonymousUtils;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    static final String USER_ID_KEY = "userId";
+    static final String BODY_KEY = "body";
+
+    EditText etMessage;
+    Button btSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ParseUser.getCurrentUser() != null) {
+            startWithCurrentUser();
+        } else {
+            login();
+        }
+    }
+
+    void startWithCurrentUser() {
+        setupMessagePosting();
+    }
+
+    void setupMessagePosting() {
+        etMessage = findViewById(R.id.etMessage);
+        btSend = findViewById(R.id.btSend);
+        btSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = etMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put(USER_ID_KEY, ParseUser.getCurrentUser().getObjectId());
+                message.put(BODY_KEY, data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null) {
+                            Toast.makeText(MainActivity.this, "Sucessfully created message on Parse", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "Failed to save message", e);
+                        }
+                    }
+                });
+                etMessage.setText(null);
+            }
+        });
+    }
+
+    void login() {
+        ParseAnonymousUtils.logIn(new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Anonymous login failed: ", e);
+                } else  {
+                    startWithCurrentUser();
+                }
+            }
+        });
     }
 }
