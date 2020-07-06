8724affc6345082861fd92f10e7af7de1138983e
Shefali Goel <shgoel@fb.com>
2020-07-06 10:27:34 -0700

    Added receiving and refreshing messages

--- app/src/main/java/com/example/simplechat/MainActivity.java
+++ app/src/main/java/com/example/simplechat/MainActivity.java
@@ -5,26 +5,41 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseAnonymousUtils;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     static final String USER_ID_KEY = "userId";
     static final String BODY_KEY = "body";
+    static final int MAX_CHAT_MESSAGES = 50;
+    static final int POLL_INTERVAL = 1000;
+
+    Handler myHandler = new android.os.Handler();
+    Runnable mRefreshMessagesRunnable = new Runnable() {
+        @Override
+        public void run() {
+            refreshMessages();
+            myHandler.postDelayed(this, POLL_INTERVAL);
+        }
+    };
 
     RecyclerView rvChat;
     ArrayList<Message> mMessages;
@@ -43,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         } else {
             login();
         }
+
+        myHandler.postDelayed(mRefreshMessagesRunnable, POLL_INTERVAL);
     }
 
     void startWithCurrentUser() {
@@ -60,6 +77,7 @@ public class MainActivity extends AppCompatActivity {
         rvChat.setAdapter(adapter);
 
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        linearLayoutManager.setReverseLayout(true);
         rvChat.setLayoutManager(linearLayoutManager);
 
         btSend.setOnClickListener(new View.OnClickListener() {
@@ -88,7 +106,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void refreshMessages() {
-
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        query.setLimit(MAX_CHAT_MESSAGES);
+        query.orderByDescending("createdAt");
+        query.findInBackground(new FindCallback<Message>() {
+            @Override
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    mMessages.addAll(messages);
+                    adapter.notifyDataSetChanged();
+                    if (mFirstLoad) {
+                        rvChat.scrollToPosition(0);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.e("message", "Error loading messages" + e);
+                }
+            }
+        });
     }
 
     void login() {
